99ba7598b0c25d164f46c422f5f0c54d174bbe8b
Orlin Kralov <orlinkralov@gmail.com>
2024-08-30 19:58:02 +0300

    Modified .gitignore + remove non-pure libraries

--- mdb.py
+++ mdb.py
@@ -1,17 +1,13 @@
 # This is the configuration file for MariaDB, as well as all the functions associated with it
 # All functions from here begin with the db_ prefix, so it's clear what they do
 
-import mariadb
-from dotenv import load_dotenv
+import pymysql
 import hashlib as hl
 from config import MDB_HOST, MDB_USER, MDB_PASSWORD, MDB_SCHEMA  # This is supposed to error out, read config-example.py
 
-load_dotenv()
-
-
 # If you're wondering where these vars come from, check .env.example
 def db_create_connection():
-    conn = mariadb.connect(
+    conn = pymysql.connect(
         host=MDB_HOST,
         user=MDB_USER,
         password=MDB_PASSWORD,
