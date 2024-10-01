d658cb70f84d73050c25e4152ad225729984d760
Orlin Kralov <orlinkralov@gmail.com>
2024-10-01 23:44:03 +0300

    update to gh repo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,19 +5,19 @@ const Navbar = () => {
     return (
         <nav>
             <div className={styles.navbarLeft}>
-                <h3 className={styles.clickable}>Tracker v0.1.0</h3>
+                <h3 className='clickable'>Tracker v0.1.0</h3>
             </div>
 
             <div className={styles.navbarCenter}>
                 <ul>
-                    <li className={styles.clickable}><a href='/'>Home</a></li>
-                    <li className={styles.clickable}><a href='/about'>About</a></li>
-                    <li className={styles.clickable}><a href='/contact'>Contact</a></li>
+                    <li className='clickable'><a href='/'>Home</a></li>
+                    <li className='clickable'><a href='/about'>About</a></li>
+                    <li className='clickable'><a href='/contact'>Contact</a></li>
                 </ul>
             </div>
 
             <div className={styles.navbarRight}>
-                <h3 className={styles.clickable}>Login</h3>
+                <h3 className='clickable'>Login</h3>
             </div>
         </nav>
     );
